bad98b3466ce0d28da74771855156fa5332c2fb7
chekin <santiisalcedo129@gmail.com>
2022-05-21 01:49:37 -0300

    obras section

--- src/components/works/Works.js
+++ src/components/works/Works.js
@@ -7,30 +7,73 @@ import styled from "styled-components";
  * Internal dependencies
  */
 import { css } from "styled-components/macro"; //eslint-disable-line
-import { Container, ContentWithPaddingXl } from "../misc/Layouts.js";
+import { Container, ContentWithPaddingXl, ThreeColumnContainer } from "../misc/Layouts.js";
 import { SectionHeading } from "../misc/Headings.js";
+import { SectionDescription as Desc } from "../misc/Typography";
+
 import { PrimaryButton as PrimaryButtonBase } from "../misc/Buttons.js";
 
 import { Works } from '../../helpers/Data'
 
+// const CardContainer = tw.div`mt-10 w-full sm:w-1/2 md:w-1/3 lg:w-1/3 `;
 
-const TabContent = tw(motion.div)`mt-6 flex flex-wrap sm:-mr-10 md:-mr-6 lg:-mr-12`;
 
-const CardContainer = tw.div`mt-10 w-full sm:w-1/2 md:w-1/3 lg:w-1/4 sm:pr-10 md:pr-6 lg:pr-12`;
-const Card = tw(motion.a)`bg-gray-200 rounded-b block max-w-xs mx-auto sm:max-w-none sm:mx-0`;
+const SectionDescription = styled(Desc)`
+    text-align: center;
+    margin: 0 auto;
+`
+
+const Columns = styled(ThreeColumnContainer)`
+    justify-content: start;
+    max-width: 1024px;
+`
+
+const CardContainer = styled.div`
+    width: 100%;
+    @media (min-width: 1024px) {
+        width: calc(33% - 1rem);
+    }
+`
+
+// const Card = tw(motion.a)`bg-gray-200 block max-w-xs mx-auto sm:max-w-none sm:mx-0`;
+const Card = tw(motion.a)`bg-gray-200 `;
+
 const CardImageContainer = styled.div`
   ${props => css`background-image: url("${props.imageSrc}");`}
-  ${tw`h-56 xl:h-64 bg-center bg-cover relative rounded-t`}
+  ${tw`h-56 xl:h-64 bg-center bg-cover relative `}
 `;
+
 
 const CardHoverOverlay = styled(motion.div)`
-  background-color: rgba(255, 255, 255, 0.5);
-  ${tw`absolute inset-0 flex justify-center items-center`}
+    background-color: #1E1D24D9;
+    height: 100%;
+    align-items: center;
+    padding: 1rem 2rem;
 `;
-const CardButton = tw(PrimaryButtonBase)`text-sm`;
+
+
+const Paragraph = styled.p`
+    font-size:12px;
+`
+
+
+const Heading = styled.h3`
+    font-size: 1rem;
+    text-align: center;
+    color: white;
+    font-weight: 700;
+
+`
+
+const CardButton = styled(PrimaryButtonBase)`
+    font-size: 0.7rem;
+`;
+
+
+
+
 
 export default () => {
-
 
 
     return (
@@ -39,7 +82,10 @@ export default () => {
                 <SectionHeading>
                     Obras
                 </SectionHeading>
-                <TabContent>
+                <SectionDescription>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo i ncidunt ex placeat modi magni quia error alias, adipisci rem similique, at omnis eligendi optio eos harum.
+                </SectionDescription>
+                <Columns>
                     {Works.map((card, index) => (
                         <CardContainer key={index}>
                             <Card className="group" href={card.url} initial="rest" whileHover="hover" animate="rest">
@@ -48,28 +94,32 @@ export default () => {
                                         variants={{
                                             hover: {
                                                 opacity: 1,
-                                                height: "auto"
                                             },
                                             rest: {
                                                 opacity: 0,
-                                                height: 0
                                             }
                                         }}
-                                        transition={{ duration: 0.3 }}
+                                        transition={{ duration: 0.4 }}
                                     >
-                                        <CardButton>Buy Now</CardButton>
+                                        <Heading>
+                                            LOREM IPSUM
+                                        </Heading>
+                                        <Paragraph>
+                                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo i ncidunt ex placeat modi magni quia error alias, adipisci rem similique, at omnis eligendi optio eos harum.
+                                        </Paragraph>
+                                        <CardButton>Lear More</CardButton>
                                     </CardHoverOverlay>
                                 </CardImageContainer>
                             </Card>
+
                         </CardContainer>
                     ))}
-                </TabContent>
+                </Columns>
             </ContentWithPaddingXl>
         </Container>
     );
 };
 
-/* This function is only there for demo purposes. It populates placeholder cards */
 const getRandomCards = () => {
     // Shuffle array
     return Works.sort(() => Math.random() - 0.5);
--- src/helpers/Data.js
+++ src/helpers/Data.js
@@ -61,7 +61,7 @@ export const cardServices = [
 export const Works = [
     {
         imageSrc:
-            "https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80",
+            "https://ld-wp73.template-help.com/wordpress/prod_11368/v2/wp-content/uploads/2021/04/img-4.jpg",
         title: "Chicken Chilled",
         content: "Chicken Main Course",
         price: "$5.99",
@@ -71,7 +71,7 @@ export const Works = [
     },
     {
         imageSrc:
-            "https://images.unsplash.com/photo-1582254465498-6bc70419b607?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80",
+            "https://ld-wp73.template-help.com/wordpress/prod_11368/v2/wp-content/uploads/2021/04/img-11.jpg",
         title: "Samsa Beef",
         content: "Fried Mexican Beef",
         price: "$3.99",
@@ -81,7 +81,7 @@ export const Works = [
     },
     {
         imageSrc:
-            "https://images.unsplash.com/photo-1565310022184-f23a884f29da?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80",
+            "https://ld-wp73.template-help.com/wordpress/prod_11368/v2/wp-content/uploads/2021/04/img-14.jpg",
         title: "Carnet Nachos",
         content: "Chilli Crispy Nachos",
         price: "$3.99",
@@ -91,7 +91,7 @@ export const Works = [
     },
     {
         imageSrc:
-            "https://images.unsplash.com/photo-1534422298391-e4f8c172dddb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80",
+            "https://ld-wp73.template-help.com/wordpress/prod_11368/v2/wp-content/uploads/2021/04/img-16.jpg",
         title: "Guacamole Mex",
         content: "Mexican Chilli",
         price: "$3.99",
@@ -101,7 +101,7 @@ export const Works = [
     },
     {
         imageSrc:
-            "https://images.unsplash.com/photo-1550461716-dbf266b2a8a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80",
+            "https://ld-wp73.template-help.com/wordpress/prod_11368/v2/wp-content/uploads/2021/04/img-13.jpg",
         title: "Chillie Cake",
         content: "Deepfried Chicken",
         price: "$2.99",
@@ -111,7 +111,7 @@ export const Works = [
     },
     {
         imageSrc:
-            "https://images.unsplash.com/photo-1476224203421-9ac39bcb3327??ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80",
+            "https://ld-wp73.template-help.com/wordpress/prod_11368/v2/wp-content/uploads/2021/04/img-17.jpg",
         title: "Nelli",
         content: "Hamburger & Fries",
         price: "$7.99",
@@ -121,7 +121,7 @@ export const Works = [
     },
     {
         imageSrc:
-            "https://images.unsplash.com/photo-1455619452474-d2be8b1e70cd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80",
+            "http://indro.iamabdus.com/v1.0/wp-content/uploads/2021/04/grant-durr-UPm28si8BRE-unsplash2.jpg.webp",
         title: "Jalapeno Poppers",
         content: "Crispy Soyabeans",
         price: "$8.99",
@@ -131,7 +131,7 @@ export const Works = [
     },
     {
         imageSrc:
-            "https://images.unsplash.com/photo-1473093226795-af9932fe5856?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80",
+            "http://indro.iamabdus.com/v1.0/wp-content/uploads/2021/06/patrick-hendry-6xeDIZgoPaw-unsplash2-1.jpg",
         title: "Cajun Chicken",
         content: "Roasted Chicken & Egg",
         price: "$7.99",
